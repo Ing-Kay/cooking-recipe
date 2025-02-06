9d75638917de12df29b0880f08f920df8b089547
Ing-Kay <156531403+Ing-Kay@users.noreply.github.com>
2025-02-06 12:26:16 +0000

    Add files via upload

---
+++ state_storage_locking/s3_ dynamodb_state.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "your region"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "your bucket name"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "your dynamodb table name"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
